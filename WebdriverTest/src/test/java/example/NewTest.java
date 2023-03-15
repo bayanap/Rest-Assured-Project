9d2a15824d3c646529605b5fd5449287bd06c22b
Bayan Ahmad Azar <52635041+bayanap@users.noreply.github.com>
2023-03-15 10:51:02 +0200

    Add files via upload

---
+++ WebdriverTest/src/test/java/example/NewTest.java
@@ -0,0 +1,28 @@
+package example;
+
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+public class NewTest {		
+	    private WebDriver driver;	
+	    @BeforeTest
+		public void beforeTest() {	
+		    driver = new ChromeDriver();  
+		}			
+		@Test				
+		public void testEasy() {	
+			driver.get("http://demo.guru99.com/test/guru99home/");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+		}	
+	
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
+}	
